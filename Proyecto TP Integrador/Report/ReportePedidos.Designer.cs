139c5fa9226b2f290907b4154e8a54ad35d77a44
thel4nzer <matiasgele2000@gmail.com>
2022-11-14 15:29:02 -0300

    Size Reportes v2

--- Proyecto TP Integrador/Report/ReportePedidos.Designer.cs
+++ Proyecto TP Integrador/Report/ReportePedidos.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource3 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource2 = new Microsoft.Reporting.WinForms.ReportDataSource();
             this.pedidosBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.ProyectoPAVIDataSet = new Proyecto_TP_Integrador.ProyectoPAVIDataSet();
             this.panel1 = new System.Windows.Forms.Panel();
@@ -65,19 +65,18 @@
             this.panel1.Controls.Add(this.lblReporesYListados);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(773, 81);
+            this.panel1.Size = new System.Drawing.Size(1225, 100);
             this.panel1.TabIndex = 0;
             // 
             // lblReporesYListados
             // 
             this.lblReporesYListados.AutoSize = true;
             this.lblReporesYListados.Font = new System.Drawing.Font("Perpetua", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblReporesYListados.Location = new System.Drawing.Point(153, 9);
-            this.lblReporesYListados.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblReporesYListados.Location = new System.Drawing.Point(343, 9);
             this.lblReporesYListados.Name = "lblReporesYListados";
-            this.lblReporesYListados.Size = new System.Drawing.Size(473, 55);
+            this.lblReporesYListados.Size = new System.Drawing.Size(597, 69);
             this.lblReporesYListados.TabIndex = 65;
             this.lblReporesYListados.Text = "REPORTE DE PEDIDOS";
             // 
@@ -90,10 +89,9 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Perpetua", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(364, 113);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(602, 142);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(98, 31);
+            this.label1.Size = new System.Drawing.Size(122, 38);
             this.label1.TabIndex = 70;
             this.label1.Text = "HASTA:";
             // 
@@ -102,47 +100,47 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Perpetua", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(110, 114);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(264, 143);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(97, 31);
+            this.label2.Size = new System.Drawing.Size(119, 38);
             this.label2.TabIndex = 69;
             this.label2.Text = "DESDE:";
             // 
             // txtFechaDesde
             // 
             this.txtFechaDesde.Font = new System.Drawing.Font("Perpetua", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtFechaDesde.Location = new System.Drawing.Point(220, 112);
-            this.txtFechaDesde.Margin = new System.Windows.Forms.Padding(2);
+            this.txtFechaDesde.Location = new System.Drawing.Point(410, 141);
+            this.txtFechaDesde.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtFechaDesde.Mask = "00/00/0000";
             this.txtFechaDesde.Name = "txtFechaDesde";
-            this.txtFechaDesde.Size = new System.Drawing.Size(124, 35);
+            this.txtFechaDesde.Size = new System.Drawing.Size(164, 42);
             this.txtFechaDesde.TabIndex = 68;
             this.txtFechaDesde.ValidatingType = typeof(System.DateTime);
             // 
             // txtFechaHasta
             // 
             this.txtFechaHasta.Font = new System.Drawing.Font("Perpetua", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtFechaHasta.Location = new System.Drawing.Point(474, 112);
-            this.txtFechaHasta.Margin = new System.Windows.Forms.Padding(2);
+            this.txtFechaHasta.Location = new System.Drawing.Point(749, 141);
+            this.txtFechaHasta.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtFechaHasta.Mask = "00/00/0000";
             this.txtFechaHasta.Name = "txtFechaHasta";
-            this.txtFechaHasta.Size = new System.Drawing.Size(124, 35);
+            this.txtFechaHasta.Size = new System.Drawing.Size(164, 42);
             this.txtFechaHasta.TabIndex = 67;
             this.txtFechaHasta.ValidatingType = typeof(System.DateTime);
             // 
             // reportPedidos
             // 
-            reportDataSource3.Name = "DatosPedidos";
-            reportDataSource3.Value = this.pedidosBindingSource;
-            this.reportPedidos.LocalReport.DataSources.Add(reportDataSource3);
+            reportDataSource2.Name = "DatosPedidos";
+            reportDataSource2.Value = this.pedidosBindingSource;
+            this.reportPedidos.LocalReport.DataSources.Add(reportDataSource2);
             this.reportPedidos.LocalReport.ReportEmbeddedResource = "Proyecto_TP_Integrador.Listados.ListadoDePedidos.rdlc";
-            this.reportPedidos.Location = new System.Drawing.Point(14, 230);
-            this.reportPedidos.Margin = new System.Windows.Forms.Padding(2);
+            this.reportPedidos.Location = new System.Drawing.Point(19, 283);
+            this.reportPedidos.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.reportPedidos.Name = "reportPedidos";
             this.reportPedidos.ServerReport.BearerToken = null;
-            this.reportPedidos.Size = new System.Drawing.Size(748, 358);
+            this.reportPedidos.Size = new System.Drawing.Size(602, 440);
             this.reportPedidos.TabIndex = 66;
+            this.reportPedidos.ZoomPercent = 75;
             this.reportPedidos.Load += new System.EventHandler(this.reportPedidos_Load);
             // 
             // lblPuesto
@@ -150,10 +148,9 @@
             this.lblPuesto.AutoSize = true;
             this.lblPuesto.Font = new System.Drawing.Font("Perpetua", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblPuesto.ForeColor = System.Drawing.Color.White;
-            this.lblPuesto.Location = new System.Drawing.Point(129, 172);
-            this.lblPuesto.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPuesto.Location = new System.Drawing.Point(289, 215);
             this.lblPuesto.Name = "lblPuesto";
-            this.lblPuesto.Size = new System.Drawing.Size(116, 31);
+            this.lblPuesto.Size = new System.Drawing.Size(143, 38);
             this.lblPuesto.TabIndex = 65;
             this.lblPuesto.Text = "ESTADO:";
             // 
@@ -161,10 +158,10 @@
             // 
             this.cmbEstado.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbEstado.FormattingEnabled = true;
-            this.cmbEstado.Location = new System.Drawing.Point(255, 172);
-            this.cmbEstado.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbEstado.Location = new System.Drawing.Point(457, 215);
+            this.cmbEstado.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cmbEstado.Name = "cmbEstado";
-            this.cmbEstado.Size = new System.Drawing.Size(135, 30);
+            this.cmbEstado.Size = new System.Drawing.Size(179, 37);
             this.cmbEstado.TabIndex = 64;
             // 
             // btnFiltrar
@@ -172,10 +169,10 @@
             this.btnFiltrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.btnFiltrar.Font = new System.Drawing.Font("Perpetua", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnFiltrar.ForeColor = System.Drawing.Color.White;
-            this.btnFiltrar.Location = new System.Drawing.Point(434, 169);
-            this.btnFiltrar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnFiltrar.Location = new System.Drawing.Point(696, 211);
+            this.btnFiltrar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnFiltrar.Name = "btnFiltrar";
-            this.btnFiltrar.Size = new System.Drawing.Size(164, 37);
+            this.btnFiltrar.Size = new System.Drawing.Size(219, 46);
             this.btnFiltrar.TabIndex = 63;
             this.btnFiltrar.Text = "FILTRAR";
             this.btnFiltrar.UseVisualStyleBackColor = false;
@@ -184,19 +181,21 @@
             // estadisticaPedidos
             // 
             this.estadisticaPedidos.LocalReport.ReportEmbeddedResource = "Proyecto_TP_Integrador.Listados.EstadisticaPedidos.rdlc";
-            this.estadisticaPedidos.Location = new System.Drawing.Point(14, 611);
+            this.estadisticaPedidos.Location = new System.Drawing.Point(632, 283);
+            this.estadisticaPedidos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.estadisticaPedidos.Name = "estadisticaPedidos";
             this.estadisticaPedidos.ServerReport.BearerToken = null;
-            this.estadisticaPedidos.Size = new System.Drawing.Size(747, 438);
+            this.estadisticaPedidos.Size = new System.Drawing.Size(565, 440);
             this.estadisticaPedidos.TabIndex = 71;
+            this.estadisticaPedidos.ZoomPercent = 75;
             this.estadisticaPedidos.Load += new System.EventHandler(this.estadisticaPedidos_Load_1);
             // 
             // ReportePedidos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(773, 1061);
+            this.ClientSize = new System.Drawing.Size(1225, 745);
             this.Controls.Add(this.estadisticaPedidos);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label2);
@@ -207,7 +206,7 @@
             this.Controls.Add(this.cmbEstado);
             this.Controls.Add(this.btnFiltrar);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
             this.Name = "ReportePedidos";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
