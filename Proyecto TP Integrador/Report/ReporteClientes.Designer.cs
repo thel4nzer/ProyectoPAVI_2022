b6261ae130d424df49f66a1924cda58803309f72
TomasGaliano <tomasga-2002@hotmail.com>
2022-11-13 23:17:54 -0300

    Proyecto Update ReporteClientes v1.3

--- Proyecto TP Integrador/Report/ReporteClientes.Designer.cs
+++ Proyecto TP Integrador/Report/ReporteClientes.Designer.cs
@@ -29,19 +29,41 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource2 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.clientesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.ProyectoPAVIDataSet = new Proyecto_TP_Integrador.ProyectoPAVIDataSet();
             this.panel1 = new System.Windows.Forms.Panel();
             this.lblReporesYListados = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.reportClientes = new Microsoft.Reporting.WinForms.ReportViewer();
-            this.ProyectoPAVIDataSet = new Proyecto_TP_Integrador.ProyectoPAVIDataSet();
-            this.clientesBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.clientesTableAdapter = new Proyecto_TP_Integrador.ProyectoPAVIDataSetTableAdapters.clientesTableAdapter();
+            this.cmbSucursal = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFechaDesde = new System.Windows.Forms.MaskedTextBox();
+            this.txtFechaHasta = new System.Windows.Forms.MaskedTextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblPuesto = new System.Windows.Forms.Label();
+            this.estadisticaClientes = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.btnFiltrar = new System.Windows.Forms.Button();
+            this.reportClientes = new Microsoft.Reporting.WinForms.ReportViewer();
+            ((System.ComponentModel.ISupportInitialize)(this.clientesBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ProyectoPAVIDataSet)).BeginInit();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.ProyectoPAVIDataSet)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.clientesBindingSource)).BeginInit();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // clientesBindingSource
+            // 
+            this.clientesBindingSource.DataMember = "clientes";
+            this.clientesBindingSource.DataSource = this.ProyectoPAVIDataSet;
+            // 
+            // ProyectoPAVIDataSet
+            // 
+            this.ProyectoPAVIDataSet.DataSetName = "ProyectoPAVIDataSet";
+            this.ProyectoPAVIDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
             // panel1
             // 
@@ -49,9 +71,9 @@
             this.panel1.Controls.Add(this.lblReporesYListados);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(869, 81);
+            this.panel1.Size = new System.Drawing.Size(868, 81);
             this.panel1.TabIndex = 0;
             // 
             // lblReporesYListados
@@ -68,59 +90,186 @@
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.Black;
-            this.panel2.Controls.Add(this.reportClientes);
+            this.panel2.Controls.Add(this.panel3);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel2.Location = new System.Drawing.Point(0, 81);
-            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(869, 402);
+            this.panel2.Size = new System.Drawing.Size(868, 932);
             this.panel2.TabIndex = 1;
-            // 
-            // reportClientes
-            // 
-            reportDataSource1.Name = "DatosClientes";
-            reportDataSource1.Value = this.clientesBindingSource;
-            this.reportClientes.LocalReport.DataSources.Add(reportDataSource1);
-            this.reportClientes.LocalReport.ReportEmbeddedResource = "Proyecto_TP_Integrador.Listados.ListadoDeClientes.rdlc";
-            this.reportClientes.Location = new System.Drawing.Point(66, 36);
-            this.reportClientes.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.reportClientes.Name = "reportClientes";
-            this.reportClientes.ServerReport.BearerToken = null;
-            this.reportClientes.Size = new System.Drawing.Size(770, 297);
-            this.reportClientes.TabIndex = 0;
-            this.reportClientes.Load += new System.EventHandler(this.reportClientes_Load);
-            // 
-            // ProyectoPAVIDataSet
-            // 
-            this.ProyectoPAVIDataSet.DataSetName = "ProyectoPAVIDataSet";
-            this.ProyectoPAVIDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // clientesBindingSource
-            // 
-            this.clientesBindingSource.DataMember = "clientes";
-            this.clientesBindingSource.DataSource = this.ProyectoPAVIDataSet;
             // 
             // clientesTableAdapter
             // 
             this.clientesTableAdapter.ClearBeforeFill = true;
             // 
+            // cmbSucursal
+            // 
+            this.cmbSucursal.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbSucursal.FormattingEnabled = true;
+            this.cmbSucursal.Location = new System.Drawing.Point(213, 78);
+            this.cmbSucursal.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbSucursal.Name = "cmbSucursal";
+            this.cmbSucursal.Size = new System.Drawing.Size(169, 30);
+            this.cmbSucursal.TabIndex = 70;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Perpetua", 19.8F);
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(51, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(218, 31);
+            this.label3.TabIndex = 68;
+            this.label3.Text = "Fecha de facturación:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Perpetua", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(526, 24);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 31);
+            this.label1.TabIndex = 67;
+            this.label1.Text = "HASTA:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Perpetua", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(272, 25);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 31);
+            this.label2.TabIndex = 66;
+            this.label2.Text = "DESDE:";
+            // 
+            // txtFechaDesde
+            // 
+            this.txtFechaDesde.Font = new System.Drawing.Font("Perpetua", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFechaDesde.Location = new System.Drawing.Point(382, 23);
+            this.txtFechaDesde.Margin = new System.Windows.Forms.Padding(2);
+            this.txtFechaDesde.Mask = "00/00/0000";
+            this.txtFechaDesde.Name = "txtFechaDesde";
+            this.txtFechaDesde.Size = new System.Drawing.Size(124, 35);
+            this.txtFechaDesde.TabIndex = 65;
+            this.txtFechaDesde.ValidatingType = typeof(System.DateTime);
+            // 
+            // txtFechaHasta
+            // 
+            this.txtFechaHasta.Font = new System.Drawing.Font("Perpetua", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFechaHasta.Location = new System.Drawing.Point(636, 23);
+            this.txtFechaHasta.Margin = new System.Windows.Forms.Padding(2);
+            this.txtFechaHasta.Mask = "00/00/0000";
+            this.txtFechaHasta.Name = "txtFechaHasta";
+            this.txtFechaHasta.Size = new System.Drawing.Size(124, 35);
+            this.txtFechaHasta.TabIndex = 64;
+            this.txtFechaHasta.ValidatingType = typeof(System.DateTime);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Black;
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.lblPuesto);
+            this.panel3.Controls.Add(this.cmbSucursal);
+            this.panel3.Controls.Add(this.estadisticaClientes);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.txtFechaDesde);
+            this.panel3.Controls.Add(this.txtFechaHasta);
+            this.panel3.Controls.Add(this.btnFiltrar);
+            this.panel3.Controls.Add(this.reportClientes);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(868, 932);
+            this.panel3.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Black;
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(48, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(333, 13);
+            this.label4.TabIndex = 73;
+            this.label4.Text = "*elegir una sucursal para visualizar el gráfico de clientes por sucursal*";
+            // 
+            // lblPuesto
+            // 
+            this.lblPuesto.AutoSize = true;
+            this.lblPuesto.Font = new System.Drawing.Font("Perpetua", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPuesto.ForeColor = System.Drawing.Color.White;
+            this.lblPuesto.Location = new System.Drawing.Point(66, 78);
+            this.lblPuesto.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPuesto.Name = "lblPuesto";
+            this.lblPuesto.Size = new System.Drawing.Size(143, 31);
+            this.lblPuesto.TabIndex = 71;
+            this.lblPuesto.Text = "SUCURSAL:";
+            // 
+            // estadisticaClientes
+            // 
+            this.estadisticaClientes.LocalReport.ReportEmbeddedResource = "Proyecto_TP_Integrador.Listados.EstadisticaClientes.rdlc";
+            this.estadisticaClientes.Location = new System.Drawing.Point(11, 497);
+            this.estadisticaClientes.Name = "estadisticaClientes";
+            this.estadisticaClientes.ServerReport.BearerToken = null;
+            this.estadisticaClientes.Size = new System.Drawing.Size(846, 414);
+            this.estadisticaClientes.TabIndex = 69;
+            this.estadisticaClientes.Load += new System.EventHandler(this.estadisticaClientes_Load);
+            // 
+            // btnFiltrar
+            // 
+            this.btnFiltrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnFiltrar.Font = new System.Drawing.Font("Perpetua", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFiltrar.ForeColor = System.Drawing.Color.White;
+            this.btnFiltrar.Location = new System.Drawing.Point(397, 75);
+            this.btnFiltrar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnFiltrar.Name = "btnFiltrar";
+            this.btnFiltrar.Size = new System.Drawing.Size(164, 37);
+            this.btnFiltrar.TabIndex = 63;
+            this.btnFiltrar.Text = "FILTRAR";
+            this.btnFiltrar.UseVisualStyleBackColor = false;
+            this.btnFiltrar.Click += new System.EventHandler(this.btnFiltrar_Click);
+            // 
+            // reportClientes
+            // 
+            reportDataSource2.Name = "DatosClientes";
+            reportDataSource2.Value = this.clientesBindingSource;
+            this.reportClientes.LocalReport.DataSources.Add(reportDataSource2);
+            this.reportClientes.LocalReport.ReportEmbeddedResource = "Proyecto_TP_Integrador.Listados.ListadoDeClientes.rdlc";
+            this.reportClientes.Location = new System.Drawing.Point(11, 145);
+            this.reportClientes.Margin = new System.Windows.Forms.Padding(2);
+            this.reportClientes.Name = "reportClientes";
+            this.reportClientes.ServerReport.BearerToken = null;
+            this.reportClientes.Size = new System.Drawing.Size(847, 338);
+            this.reportClientes.TabIndex = 0;
+            this.reportClientes.Load += new System.EventHandler(this.reportClientes_Load);
+            // 
             // ReporteClientes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(869, 483);
+            this.ClientSize = new System.Drawing.Size(868, 1013);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "ReporteClientes";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Reporte de Clientes";
             this.Load += new System.EventHandler(this.ReporteClientes_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.clientesBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ProyectoPAVIDataSet)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.ProyectoPAVIDataSet)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.clientesBindingSource)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -129,10 +278,21 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
-        private Microsoft.Reporting.WinForms.ReportViewer reportClientes;
         private System.Windows.Forms.Label lblReporesYListados;
         private System.Windows.Forms.BindingSource clientesBindingSource;
         private ProyectoPAVIDataSet ProyectoPAVIDataSet;
         private ProyectoPAVIDataSetTableAdapters.clientesTableAdapter clientesTableAdapter;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblPuesto;
+        private System.Windows.Forms.ComboBox cmbSucursal;
+        private Microsoft.Reporting.WinForms.ReportViewer estadisticaClientes;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MaskedTextBox txtFechaDesde;
+        private System.Windows.Forms.MaskedTextBox txtFechaHasta;
+        private System.Windows.Forms.Button btnFiltrar;
+        private Microsoft.Reporting.WinForms.ReportViewer reportClientes;
     }
 }
--- Proyecto TP Integrador/Report/ReporteClientes.cs
+++ Proyecto TP Integrador/Report/ReporteClientes.cs
@@ -17,6 +17,37 @@ namespace Proyecto_TP_Integrador
         public ReporteClientes()
         {
             InitializeComponent();
+            CargarComboSucursales();
+        }
+
+        private void CargarComboSucursales()
+        {
+            string cadenaConexion = System.Configuration.ConfigurationManager.AppSettings["CadenaBD"];
+            SqlConnection cn = new SqlConnection(cadenaConexion);
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                string consulta = "SELECT NomSucursal FROM sucursales";
+                cmd.Parameters.Clear();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = consulta;
+
+                cn.Open();
+                cmd.Connection = cn;
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr != null && dr.Read())
+                {
+                    cmbSucursal.Items.Add(dr["NomSucursal"].ToString());
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
         }
 
         private void ReporteClientes_Load(object sender, EventArgs e)
@@ -25,6 +56,7 @@ namespace Proyecto_TP_Integrador
             //this.clientesTableAdapter.Fill(this.ProyectoPAVIDataSet.clientes);
 
             this.reportClientes.RefreshReport();
+            this.estadisticaClientes.RefreshReport();
         }
 
         private void reportClientes_Load(object sender, EventArgs e)
@@ -69,5 +101,154 @@ namespace Proyecto_TP_Integrador
                 cn.Close();
             }
         }
+
+        public static DateTime? TryParse(string text)
+        {
+            DateTime date;
+            if (DateTime.TryParse(text, out date))
+            {
+                return date;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private void btnFiltrar_Click(object sender, EventArgs e)
+
+        {
+            DateTime? desde = TryParse(txtFechaDesde.Text);
+            DateTime? hasta = TryParse(txtFechaHasta.Text);
+
+            if (desde == null)
+            {
+                txtFechaDesde.Text = "";
+            }
+
+            if (hasta == null)
+            {
+                txtFechaHasta.Text = "";
+            }
+
+
+            DataTable tablareporte = new DataTable();
+            string sucu = cmbSucursal.Text;
+            tablareporte = CargarClientesFiltro(desde, hasta, sucu);
+
+            reportClientes.LocalReport.DataSources.Clear();
+            reportClientes.LocalReport.DataSources.Add(new ReportDataSource("DatosClientes", tablareporte));
+            reportClientes.RefreshReport();
+
+
+            DataTable tablaGrafico = new DataTable();
+            
+            tablaGrafico = CargarEstadisticaClientes(sucu);
+            estadisticaClientes.LocalReport.DataSources.Clear();
+            estadisticaClientes.LocalReport.DataSources.Add(new ReportDataSource("DatosEstadisticosClientes", tablaGrafico));
+            estadisticaClientes.RefreshReport();
+
+
+        }
+
+        public static DataTable CargarClientesFiltro(DateTime? desde, DateTime? hasta, string nombre)
+        {
+
+            string cadenaConexion = System.Configuration.ConfigurationManager.AppSettings["CadenaBD"];
+            SqlConnection cn = new SqlConnection(cadenaConexion);
+            try
+            {
+
+                SqlCommand cmd = new SqlCommand();
+                string consulta = "SELECT c.IdCliente, c.NomCliente, c.ApeCliente, c.TipoDoc, c.NumeroDoc, c.IdLocalidad FROM clientes c INNER JOIN " +
+                                    "facturas f ON c.IdCliente=f.IdCliente INNER JOIN sucursales suc ON f.IdSucursal = suc.IdSucursal WHERE f.IdFactura > 0";
+
+
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@name", nombre);
+
+                if (desde.HasValue)
+                {
+                    consulta += " AND f.Fecha >= CONVERT(char(10), '" + desde.ToString() + "', 111) ";
+                }
+                if (hasta.HasValue)
+                {
+                    consulta += " AND f.Fecha <= CONVERT(char(10), '" + hasta.ToString() + "', 111) ";
+                }
+                if (nombre != "")
+                {
+                    consulta += " AND suc.NomSucursal like @name";
+                }
+
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = consulta;
+
+                cn.Open();
+                cmd.Connection = cn;
+                DataTable tabla = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(tabla);
+
+                return tabla;
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+
+        }
+
+        public static DataTable CargarEstadisticaClientes(string nombre)
+        {
+
+            string cadenaConexion = System.Configuration.ConfigurationManager.AppSettings["CadenaBD"];
+            SqlConnection cn = new SqlConnection(cadenaConexion);
+            try
+            {
+
+                SqlCommand cmd = new SqlCommand();
+                string consulta = "SELECT c.ApeCliente as Nombre, COUNT(f.IdFactura) as Cantidad FROM sucursales suc " +
+                                    "INNER JOIN facturas f ON suc.IdSucursal=f.IdSucursal INNER JOIN clientes c ON c.IdCliente = f.IdCliente WHERE suc.NomSucursal LIKE @name " +
+                                    "GROUP BY c.ApeCliente";
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@name", nombre);
+
+
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = consulta;
+
+                cn.Open();
+                cmd.Connection = cn;
+                DataTable tabla = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(tabla);
+
+                return tabla;
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+        private void estadisticaClientes_Load(object sender, EventArgs e)
+        {
+            DataTable tablaGrafico = new DataTable();
+            estadisticaClientes.LocalReport.DataSources.Clear();
+            estadisticaClientes.LocalReport.DataSources.Add(new ReportDataSource("DatosEstadisticosClientes", tablaGrafico));
+            estadisticaClientes.RefreshReport();
+        }
     }
 }
+
